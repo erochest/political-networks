dc3c06019e7f1f973305191bb421891b7f689258
Eric Rochester <erochest@virginia.edu>
2014-02-10 11:38:29 -0500

    Download political organizations in the US.

--- peeps.py
+++ peeps.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 
+import json
 from linkedin import linkedin
+import time
 from pprint import pprint
 
 from keys import *
@@ -11,26 +13,49 @@ RETURN_URL   = 'http://localhost:8000'
 
 # Organizing for Action
 COMPANY_NAME = 'organizing-for-action'
+POLITICAL_ORGANIZATION = 107
 
-authentication = linkedin.LinkedInDeveloperAuthentication(
-        API_KEY, API_SECRET,
-        USER_TOKEN, USER_SECRET,
-        RETURN_URL, linkedin.PERMISSIONS.enums.values()
-        )
+SELECTORS=['companies:(id,name,universal-name,website-url,company-type,industries,locations)']
+PARAMS={
+    'facet': [
+        'industry,{}'.format(POLITICAL_ORGANIZATION),
+        'location,us',
+        ],
+    }
 
-# Pass it in to the app...
 
-application = linkedin.LinkedInApplication(authentication)
+def page_companies(app, selectors, params, start=0):
+    print('page_companies start={}'.format(start))
+    sparams = params.copy()
+    sparams['start'] = start
+    result = app.search_company(
+            selectors=selectors,
+            params=sparams,
+            )
+    result = result['companies']
 
-# Use the app....
+    values = result['values']
+    for v in values:
+        yield v
+    time.sleep(1)
 
-companies = application.get_companies(universal_names=[COMPANY_NAME])
-pprint(companies)
-for co_info in companies['values']:
-    updates = application.get_company_updates(co_info['id'], params={'count': 2})
-    pprint(updates)
+    if result['_total'] > start + len(values):
+        for v in page_companies(app, selectors, params, start=start+len(values)):
+            yield v
 
-# updates = application.get_company_updates(COMPANY_ID, params={'count': 2})
-# pprint(updates)
 
+def main():
+    authentication = linkedin.LinkedInDeveloperAuthentication(
+            API_KEY, API_SECRET,
+            USER_TOKEN, USER_SECRET,
+            RETURN_URL, linkedin.PERMISSIONS.enums.values()
+            )
+    application = linkedin.LinkedInApplication(authentication)
+
+    with open('industry-pol.json', 'wb') as f:
+        json.dump(list(page_companies(application, SELECTORS, PARAMS, start=0)), f)
+
+
+if __name__ == '__main__':
+    main()
 
